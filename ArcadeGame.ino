3123adb06701d653078a16edde321701e8eb7cdc
Håkon Årdal <hakon.ardal@gmail.com>
2024-09-03 22:33:03 +0200

    first working prototype

--- ArcadeGame.ino
+++ ArcadeGame.ino
@@ -1,51 +1,90 @@
-const int swingProbRange[2] = {0, 1};
-const int fakeSwingProbRange[2] = {2, 4};
-int angleShift = 0;
+#include <Servo.h>
 
+Servo hammer;
+
+int buttonPin = 6;
+int hammerStartPos = 0; // deg angle
+int hammerEndPos = 60;
+
+bool hammerDown = false;
+bool playerFooled = false;
 
 unsigned long randNumber;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 
-  randomSeed(analogRead(0));
+  pinMode(buttonPin, INPUT_PULLUP);
 
+  randomSeed(analogRead(0)); // Set seed for random() function
+  hammer.attach(9);  // Set servo to use pin 9
+  hammer.write(hammerStartPos);  // Set hammer to start position
 
 }
 
+// Aproach 2
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  // Generate random number
-  randNumber = random(100);
-
-
-
-
-  if(randNumber >= swingProbRange[0] && randNumber <= swingProbRange[1]){
-    // Turn hammer down
-    swingHammer();
-
-  }
-  else if(randNumber >= fakeSwingProbRange[0] && randNumber <= fakeSwingProbRange[1]){
-    // Turn hammer just a bit, the go back
-    fakeSwing();
+  while (digitalRead(buttonPin) == HIGH) {
+    // Wait for button to be pressed
+    delay(200);
   }
 
+  // Game starts. TODO: green LED when starting?
+  while(!hammerDown) {
+    
+    int randInt = random(1, 50);
+
+    if (randInt == 1) {
+      swingHammer();
+    }
+    else if (randInt == 2) {
+      fakeSwing();
+    }
+
+    // Check if button is released
+    if(digitalRead(buttonPin) == HIGH) { // button released
+      if (!hammerDown) { // Fooled
+        playerFooled = true;
+        break;
+      }
+      else {
+        hammerDown = true;
+      }
+    }
+
+    delay(100); 
+  }
+  // Game is done. Display red indicator if player got fooled
+  // Remember to reset hammer to start position
+
+  delay(1000);
+  hammer.write(hammerStartPos);
+
+  Serial.println("Game over");
+
+  // Reset parameters
+  hammerDown = false;
+  playerFooled = false;
+
+}
 
 
+bool isFake() {
+  return random(0, 2) == 1; // generates either 0 or 1. 50% chance
+}
 
+void swingHammer() {
+  hammer.write(hammerEndPos);
+  hammerDown = true;
+  Serial.println("Real swing");
+}
+
+void fakeSwing() {
+  hammer.write(hammerStartPos + 2);
   delay(100);
-
-
-}
-
-void fakeSwing(){
+  hammer.write(hammerStartPos);
+  Serial.println("Fake swing");
 
 }
 
-
-void swingHammer(){
-
-}
