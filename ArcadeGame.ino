348040bf3307d2a6350ae467e7755d5fd9f19d4f
Håkon Årdal <hakon.ardal@gmail.com>
2024-09-05 11:46:23 +0200

    added reaction time

--- ArcadeGame.ino
+++ ArcadeGame.ino
@@ -2,9 +2,10 @@
 
 Servo hammer; // Init servo object
 
-// init pins:
-int buttonPin = 6;
-int potentiometerPin = A0;
+// pins:
+const int buttonPin = 6;
+const int servoPin = 9;
+const int potentiometerPin = A0;
 
 // tunable parameters: 
 int hammerStartPos = 0; // deg angle
@@ -14,6 +15,8 @@ int hammerEndPos = 60;
 bool hammerDown = false;
 bool playerFooled = false;
 unsigned long randNumber;
+unsigned long swingTime;
+unsigned long reactionTime;
 
 void setup() {
   Serial.begin(9600);
@@ -21,27 +24,26 @@ void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
 
   randomSeed(analogRead(0)); // Set seed for random() function
-  hammer.attach(9);  // Set servo to use pin 9
+  hammer.attach(servoPin);  // Set servo to use pin 9
   hammer.write(hammerStartPos);  // Set hammer to start position
 
 }
 
-// Aproach 2
+
 void loop() {
 
-  while (digitalRead(buttonPin) == HIGH) {
-    // Wait for button to be pressed
-    hammerStartPos = updateHammerStartPos();
+  while (!isButtonDown()) { // Wait for button to be pressed
+    hammerStartPos = readPotentiometerPosition();
     hammer.write(hammerStartPos);
     delay(100);
   }
 
   // Game starts. TODO: green LED when starting?
-  while(!hammerDown) {
+  while(true) {
     
-    int randInt = random(1, 100);
+    randNumber = random(1, 5000);
 
-    if (randInt == 1) {
+    if (randNumber == 1) {
       if(isFake()) {
         fakeSwing();
       }
@@ -50,37 +52,51 @@ void loop() {
       }
     }
     
-
     // Check if button is released
-    if(digitalRead(buttonPin) == HIGH) { // button released
-      if (!hammerDown) { // Fooled
+    if(!isButtonDown() && !hammerDown) { // button released when hammer is still up
         playerFooled = true;
         break;
-      }
-      else {
-        hammerDown = true;
-      }
+    }
+    else if(hammerDown && isButtonDown) { // Hammer swings, button still down
+      // update reaction time
+      reactionTime = millis() - swingTime;
+      
+      if (!isButtonDown()) break; // break when button is released
     }
 
-    delay(50); 
+    delay(1); 
   }
 
   // Game is done. Print results
-  
+  printResults();
+  delay(1000);
+
+  resetGame();
+  delay(1000);
+}
+
+
+void printResults() {
   Serial.println("Game over");
 
   if(playerFooled) {
     Serial.println("You got fooled!");
+  } else {
+    Serial.print("Reaction Time: ");
+    Serial.print(reactionTime);
+    Serial.println(" ms");
   }
+}
 
+
+void resetGame() {
   // Return hammer to start pos
-  delay(1000);
   hammer.write(hammerStartPos);
 
   // Reset parameters
   hammerDown = false;
   playerFooled = false;
-  delay(1000);
+  reactionTime = 0;
 }
 
 
@@ -91,18 +107,23 @@ bool isFake() {
 void swingHammer() {
   hammer.write(hammerEndPos);
   hammerDown = true;
+  swingTime = millis(); // Record the time when hammer swings
   Serial.println("Real swing");
 }
 
 void fakeSwing() {
-  hammer.write(hammerStartPos + 2);
+  hammer.write(hammerStartPos + 4);
   delay(100);
   hammer.write(hammerStartPos);
   Serial.println("Fake swing");
 }
 
-int updateHammerStartPos() {
+int readPotentiometerPosition() {
   int potentiometerVal = analogRead(potentiometerPin);
   int scaledVal = map(potentiometerVal, 0, 1023, 0, 60); // Potentiometer from 0 to 60 deg
   return scaledVal;
+}
+
+bool isButtonDown() {
+  return digitalRead(buttonPin) == LOW;
 }
